fd2eed05670af3c7847fd8239794aacb570dc390
Zach Sorenson <zws@DN527k8u.SUNet>
2015-08-02 09:08:14 -0400

    hidden nav tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-    $(window).scroll(function(){
-        var top = $(this).scrollTop();
-        var btm = $(document).height() - $(this).height() - $(this).scrollTop();
-        if (top > 80 && btm > 240) {
-            $('.navbar').addClass("navbar-hidden");
-        } else {
-            $('.navbar').removeClass("navbar-hidden");
-        }
-    });
+    var navbar = $('.navbar');
+    if (window.location.pathname !== "/") {
+        $(window).scroll(function(){
+            var top = $(this).scrollTop();
+            var btm = $(document).height() - $(this).height() - $(this).scrollTop();
+            if (top > 80 || btm < 240 || window.location.pathname === "/") {
+                navbar.addClass("navbar-hidden");
+            } else {
+                navbar.removeClass("navbar-hidden");
+            }
+        });
+    }
 })
