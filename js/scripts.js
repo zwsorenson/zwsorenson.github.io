1a0a94e95c21f0bc2b05524f3d1723f09b4cbb01
Zach Sorenson <zws@DN527k8u.SUNet>
2015-08-02 10:35:30 -0400

    edited boy scouts post

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         $(window).scroll(function(){
             var top = $(this).scrollTop();
             var btm = $(document).height() - $(this).height() - $(this).scrollTop();
-            if (top > 80 || btm < 240 || window.location.pathname === "/") {
+            if ((top > 80) && (btm > 240)) {
                 navbar.addClass("navbar-hidden");
             } else {
                 navbar.removeClass("navbar-hidden");
